91c2a97987df45dae25192f6b81e6d43519dd06c
DineshChirnanchu <Dinesh516ch@outlook.com>
2020-02-18 19:44:49 +0530

    initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyUnittest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(3-2, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(12-9, 2+1);
+        }
+    }
+}
